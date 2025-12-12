2075bc1a474a36b5a3687376c801c7b826ceb63c
CHOUCHANE Rayan <rayan.chouchane@efrei.net>
2025-12-12 13:41:32 +0100

    refactor: clean code

--- ethiens_sme/route/user_route.py
+++ ethiens_sme/route/user_route.py
@@ -13,31 +13,14 @@ from flask_jwt_extended import (
 user = Blueprint("user", __name__, url_prefix="/user")
 
 
-@user.route("/test", methods=["GET"])
-def register():
-    """Hello World endpoint"""
-    try:
-        response = jsonify(message="HELLO_WORLD"), 200
-        return response
-
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
-
-
 @user.route("/auth", methods=["POST"])
 def authenticate():
     """Authenfication endpoint"""
     json_object = request.json
     try:
         user_bo = user_service.authenticate(json_object.get("login", None), json_object.get("password", None))
-
         response = make_response(jsonify(message="AUTHENTIFIED_SUCCESSFULLY", pseudo_verified=user_bo.pseudo))
-
-        # --- CORRECTION ICI ---
-        # On passe directement l'ID converti en string
-        # Au lieu de {"id": user_bo.id}
         user_id_str = str(user_bo.id)
-
         access_token = create_access_token(identity=user_id_str)
         set_access_cookies(response, access_token)
 
